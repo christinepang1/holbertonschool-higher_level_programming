32e93feda768c313917df49de2a22b8f1bc0ba5f
christinepang1 <christinezpang@gmail.com>
2019-12-17 21:07:31 +0000

    Typo

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     new = my_list[:]
     for i in range(len(new)):
         if idx < 0 or idx >= len(my_list):
-            return m_list
+            return my_list
         else:
             new[idx] = element
             return new
