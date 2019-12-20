9ef1982217128b0cdbbc4b21fd1159e7b3803989
christinepang1 <christinezpang@gmail.com>
2019-12-20 07:01:15 +0000

    Try set function

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     my_list.sort()
-    sum = my_list[0]
-    for i in range(len(my_list) - 1):
-        if (my_list[i] != my_list[i+1]):
-            sum += my_list[i+1]
+    sum = 0
+    for i in set(my_list):
+        sum += i
     return sum
