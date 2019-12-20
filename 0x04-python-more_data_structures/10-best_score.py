652ed3cccbe0736344d983c96c6fe180b0d30d2d
christinepang1 <christinezpang@gmail.com>
2019-12-20 06:51:20 +0000

    Edge case

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
     this = list(a_dictionary.keys())[0]
     for keys in a_dictionary.keys():
