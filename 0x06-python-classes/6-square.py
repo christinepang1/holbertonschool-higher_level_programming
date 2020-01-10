90bbd6439ff36bf51184c5023ce38fc779a8b42b
christinepang1 <christinezpang@gmail.com>
2020-01-10 07:21:26 +0000

    Added else statement

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -36,7 +36,8 @@ class Square():
     def my_print(self):
         if self.__size == 0:
             print()
-        print("\n" * self.__position[1], end="")
-        for i in range(self.__size):
-            print(" " * self.__position[0], end="")
-            print("#" * self.__size)
+        else:
+            print("\n" * self.__position[1], end="")
+            for i in range(self.__size):
+                print(" " * self.__position[0], end="")
+                print("#" * self.__size)
