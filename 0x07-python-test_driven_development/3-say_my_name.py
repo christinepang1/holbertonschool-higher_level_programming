9909a94767ed4aa86cd645999b5e52dd2ec89d18
christinepang1 <christinezpang@gmail.com>
2020-01-13 08:28:34 +0000

    Format specifiers

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -8,7 +8,6 @@ def say_my_name(first_name, last_name=""):
     """
     Function will print strings of first_name and last_name
     """
-
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
