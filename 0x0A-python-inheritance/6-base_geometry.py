98339e3eef2ceaeb05ee194555de53adffdcb253
christinepang1 <christinezpang@gmail.com>
2020-01-21 03:50:52 +0000

    Define area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Empty Class BaseGeometry"""
+    def area(self):
+        """Area"""
+        raise Exception("area() is not implemented")
