8dbaceeebd539c99e8ccea86802e95abffbbde35
christinepang1 <christinezpang@gmail.com>
2020-01-21 22:42:30 +0000

    Return student's dictionary representations

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Class Student"""
+
+
+class Student:
+    """Class Student"""
+    def __init__(self, first_name, last_name, age):
+        """Init Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Returns dictionary description of self"""
+        return self.__dict__
