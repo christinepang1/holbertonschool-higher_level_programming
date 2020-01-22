f2b90f0e1d5119029d086c2301109bd37be4b003
christinepang1 <christinezpang@gmail.com>
2020-01-22 04:04:52 +0000

    Retrieve dictionary representation of a student

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Class Student"""
+
+
+class Student:
+    """Class Student"""
+    def __init__(self, first_name, last_name, age):
+        """Init Student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Returns dictionary description of self"""
+        return self.__dict__
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a student instance"""
+        if not isinstance(attrs, list):
+            return self.__dict__
+        if attrs is None:
+            return self.__dict__
+        dict = {}
+        for i in attrs:
+            if i in self.__dict__.keys():
+                dict[i] = self.__dict__[i]
+        return dict
