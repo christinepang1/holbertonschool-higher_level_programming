6f571fa47bf85057f13e5582b8edb0ebe3dd6d39
christinepang1 <christinezpang@gmail.com>
2020-01-27 22:52:31 +0000

    Return JSON string representation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """Base Class"""
 
 
+import json
+
+
 class Base:
     """Base Class"""
 
@@ -15,3 +18,9 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Returns JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
