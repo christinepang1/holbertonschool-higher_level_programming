2b52f1e013cabaad7dab65f53270716568b82ad2
christinepang1 <christinezpang@gmail.com>
2020-02-24 07:12:55 +0000

    Added main files

---
+++ 0x07-python-test_driven_development/main/4-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+print_square = __import__('4-print_square').print_square
+
+print_square(20)
+print("")
+print_square(10)
+print("")
---
+++ 0x07-python-test_driven_development/main/5-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+text_indentation = __import__('5-text_indentation').text_indentation
+
+a = "Pichu:"
+b = "Pikachu."
+c = "Raichu?"
+
+text_indentation(a * 2)
