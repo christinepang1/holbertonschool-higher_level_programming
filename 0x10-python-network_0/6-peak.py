7d0268acf5d60a453439fd0ae7c9c01d7eb94fdd
christinepang1 <christinezpang@gmail.com>
2020-04-08 04:43:55 +0000

    Implemented recursion

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,11 +5,8 @@ from math import floor
 
 def find_peak(list_of_integers):
     """Finds a peak in a list"""
-    if list_of_integers is None:
+    if list_of_integers is None or list_of_integers == []:
         return None
-
-    #  if array is size of 1 or 2
-
     n = len(list_of_integers)
     if n == 0:
         return None
@@ -18,15 +15,16 @@ def find_peak(list_of_integers):
     if n == 2:
         return max(list_of_integers)
 
-    start = 0
-    end = n - 1
+    return helper(list_of_integers, 0, len(list_of_integers) - 1)
 
-    while (start <= end):
-        m = (end + start) // 2
-        if (m == 0 or list_of_integers[m - 1] <= list_of_integers[m]) and (
-                m == n - 1 or list_of_integers[m] >= list_of_integers[m + 1]):
-            return list_of_integers[m]
-        elif m > 0 and list_of_integers[m - 1] > list_of_integers[m]:
-            end = m - 1
-        else:
-            start = m + 1
+
+def helper(list_of_integers, start, end):
+    """Updating the mid"""
+    m = (end + start) // 2
+    if (m == 0 or list_of_integers[m - 1] <= list_of_integers[m]) and (m == len(
+            list_of_integers) - 1 or list_of_integers[m] >= list_of_integers[m + 1]):
+        return list_of_integers[m]
+    elif m > 0 and list_of_integers[m - 1] > list_of_integers[m]:
+        return helper(list_of_integers, 0, m)
+    else:
+        return helper(list_of_integers, m + 1, end)
