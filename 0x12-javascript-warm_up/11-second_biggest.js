3c813561a235c2ac606607b9bf73530571364a43
christinepang1 <christinezpang@gmail.com>
2020-05-06 05:04:47 +0000

    Fixed length

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const input = process.argv;
-if (input.length < 4) {
+const input = process.argv.length - 2;
+if (input < 2) {
   console.log(0);
 } else {
-  const array = input.slice(2);
+  const array = process.argv.slice(2);
   const sorted = array.sort();
   console.log(array[sorted.length - 2]);
 }
